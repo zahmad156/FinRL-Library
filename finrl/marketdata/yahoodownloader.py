b6def98ee40acebfd0b9338c467ed07512a0020c
Zarrar <69991738+zahmad156@users.noreply.github.com>
2024-05-26 19:18:26 +0500

    Update yahoodownloader.py
    copy paste full code from current finnrl yahoo donwloader
    https://github.com/AI4Finance-Foundation/FinRL/tree/master

--- finrl/marketdata/yahoodownloader.py
+++ finrl/marketdata/yahoodownloader.py
@@ -2,6 +2,8 @@
 Yahoo Finance API
 """
 
+from __future__ import annotations
+
 import pandas as pd
 import yfinance as yf
 
@@ -13,11 +15,11 @@ class YahooDownloader:
     Attributes
     ----------
         start_date : str
-            start date of the data (modified from config.py)
+            start date of the data (modified from neofinrl_config.py)
         end_date : str
-            end date of the data (modified from config.py)
+            end date of the data (modified from neofinrl_config.py)
         ticker_list : list
-            a list of stock tickers (modified from config.py)
+            a list of stock tickers (modified from neofinrl_config.py)
 
     Methods
     -------
@@ -25,63 +27,79 @@ class YahooDownloader:
         Fetches data from yahoo API
 
     """
-    def __init__(self, 
-        start_date:str,
-        end_date:str,
-        ticker_list:list):
 
+    def __init__(self, start_date: str, end_date: str, ticker_list: list):
         self.start_date = start_date
         self.end_date = end_date
         self.ticker_list = ticker_list
 
-
-    def fetch_data(self) -> pd.DataFrame:
+    def fetch_data(self, proxy=None) -> pd.DataFrame:
         """Fetches data from Yahoo API
         Parameters
         ----------
 
         Returns
         -------
-        `pd.DataFrame` 
+        `pd.DataFrame`
             7 columns: A date, open, high, low, close, volume and tick symbol
             for the specified stock ticker
         """
         # Download and save the data in a pandas DataFrame:
         data_df = pd.DataFrame()
+        num_failures = 0
         for tic in self.ticker_list:
-            temp_df = yf.download(tic, start=self.start_date, end=self.end_date)
-            temp_df['tic'] = tic
-            data_df=data_df.append(temp_df)
+            temp_df = yf.download(
+                tic, start=self.start_date, end=self.end_date, proxy=proxy
+            )
+            temp_df["tic"] = tic
+            if len(temp_df) > 0:
+                # data_df = data_df.append(temp_df)
+                data_df = pd.concat([data_df, temp_df], axis=0)
+            else:
+                num_failures += 1
+        if num_failures == len(self.ticker_list):
+            raise ValueError("no data is fetched.")
         # reset the index, we want to use numbers as index instead of dates
-        data_df=data_df.reset_index()
+        data_df = data_df.reset_index()
         try:
             # convert the column names to standardized names
-            data_df.columns = ['date','open','high','low','close','adjcp','volume','tic']
+            data_df.columns = [
+                "date",
+                "open",
+                "high",
+                "low",
+                "close",
+                "adjcp",
+                "volume",
+                "tic",
+            ]
             # use adjusted close price instead of close price
-            data_df['close'] = data_df['adjcp']
+            data_df["close"] = data_df["adjcp"]
             # drop the adjusted close price column
-            data_df = data_df.drop('adjcp', 1)
+            data_df = data_df.drop(labels="adjcp", axis=1)
         except NotImplementedError:
             print("the features are not supported currently")
-
+        # create day of the week column (monday = 0)
+        data_df["day"] = data_df["date"].dt.dayofweek
         # convert date to standard string format, easy to filter
-        data_df['date']=data_df.date.apply(lambda x: x.strftime('%Y-%m-%d'))
-        # drop missing data 
+        data_df["date"] = data_df.date.apply(lambda x: x.strftime("%Y-%m-%d"))
+        # drop missing data
         data_df = data_df.dropna()
         data_df = data_df.reset_index(drop=True)
         print("Shape of DataFrame: ", data_df.shape)
-        #print("Display DataFrame: ", data_df.head())
+        # print("Display DataFrame: ", data_df.head())
+
+        data_df = data_df.sort_values(by=["date", "tic"]).reset_index(drop=True)
 
         return data_df
 
-
-    def select_equal_rows_stock(df):
-        df_check=df.tic.value_counts()
-        df_check=pd.DataFrame(df_check).reset_index()
-        df_check.columns = ['tic','counts']
+    def select_equal_rows_stock(self, df):
+        df_check = df.tic.value_counts()
+        df_check = pd.DataFrame(df_check).reset_index()
+        df_check.columns = ["tic", "counts"]
         mean_df = df_check.counts.mean()
-        equal_list=list(df.tic.value_counts() >= mean_df)
+        equal_list = list(df.tic.value_counts() >= mean_df)
         names = df.tic.value_counts().index
         select_stocks_list = list(names[equal_list])
-        df=df[df.tic.isin(select_stocks_list)]
+        df = df[df.tic.isin(select_stocks_list)]
         return df
